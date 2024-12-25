dceeb9fedf3cf73254ecdff1ee116def1624e440
Çağlar Kutlu Yiğit <caglarkutluyigit@gmail.com>
2024-12-25 23:23:01 +0300

    matrix system fix

--- Assets/cky/cky - Matrix Creator/Editor/MatrixCreatorManagerEditor.cs
+++ Assets/cky/cky - Matrix Creator/Editor/MatrixCreatorManagerEditor.cs
@@ -1,23 +1,23 @@
-//using UnityEditor;
-//using UnityEngine;
+using UnityEditor;
+using UnityEngine;
 
-//namespace cky.MatrixCreation
-//{
-//    [CustomEditor(typeof(MatrixCreatorManager))]
-//    public class MatrixCreatorManagerEditor : Editor
-//    {
-//        public override void OnInspectorGUI()
-//        {
-//            DrawDefaultInspector();
+namespace cky.MatrixCreation
+{
+    [CustomEditor(typeof(MatrixCreatorManager))]
+    public class MatrixCreatorManagerEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
 
-//            MatrixCreatorManager mcm = (MatrixCreatorManager)target;
+            MatrixCreatorManager mcm = (MatrixCreatorManager)target;
 
-//            GUILayout.Space(75);
-//            if (GUILayout.Button("Matrix Set"))
-//            {
-//                mcm.MatrixSet();
-//            }
-//            GUILayout.Space(75);
-//        }
-//    }
-//}
+            GUILayout.Space(75);
+            if (GUILayout.Button("Matrix Set"))
+            {
+                mcm.MatrixSet();
+            }
+            GUILayout.Space(75);
+        }
+    }
+}
--- Assets/cky/cky - Matrix Creator/MatrixCell.cs
+++ Assets/cky/cky - Matrix Creator/MatrixCell.cs
@@ -1,104 +1,132 @@
-//using EZ_Pooling;
-//using System.Collections.Generic;
-//using UnityEngine;
+using CKY_Pooling;
+using System.Collections.Generic;
+using UnityEngine;
 
-//namespace cky.MatrixCreation
-//{
-//    [System.Serializable]
-//    public class ItemTransforms
-//    {
-//        public List<Transform> items = new List<Transform>();
-//    }
+namespace cky.MatrixCreation
+{
+    [System.Serializable]
+    public class ItemTransforms
+    {
+        public List<Transform> items = new List<Transform>();
+    }
 
-//    public class MatrixCell : MonoBehaviour
-//    {
-//        [Space(5)]
-//        public MatrixCreatorManager Manager;
+    public class MatrixCell : MonoBehaviour
+    {
+        [Space(5)]
+        public MatrixCreatorManager Manager;
 
-//        [Space(5)]
-//        public int I;
-//        public int J;
-//        public List<MatrixCell> neighbours = new List<MatrixCell>();
+        [Space(5)]
+        public int I;
+        public int J;
+        public List<MatrixCell> neighbours = new List<MatrixCell>();
 
-//        [Space(5)]
-//        [SerializeField] ItemTransforms[] itemTransforms_s;
+        [Space(5)]
+        [SerializeField] ItemTransforms[] itemTransforms_s;
 
-//        [SerializeField] ItemIndexes[] itemIndexes_s;
-//        bool _active;
+        [SerializeField] ItemIndexes[] itemIndexes_s;
+        bool _active;
 
-//        public void Init(MatrixCreatorManager manager, int i, int j)
-//        {
-//            Manager = manager;
-//            I = i;
-//            J = j;
+        public void Init(MatrixCreatorManager manager, int i, int j)
+        {
+            Manager = manager;
+            I = i;
+            J = j;
 
-//            itemIndexes_s = new ItemIndexes[manager.controllers.Length];
+            itemIndexes_s = new ItemIndexes[manager.controllers.Length];
 
-//            for (int k = 0; k < itemIndexes_s.Length; k++)
-//            {
-//                //Debug.Log($"{I} - {J}");
-//                itemIndexes_s[k] = Manager.controllers[k].Settings.cells_ItemIndexes[I * Manager.Dimension_J + J];
-//            }
-//            itemTransforms_s = new ItemTransforms[manager.controllers.Length];
-//            for (int k = 0; k < itemTransforms_s.Length; k++)
-//            {
-//                itemTransforms_s[k] = new ItemTransforms();
-//            }
-//        }
+            for (int k = 0; k < itemIndexes_s.Length; k++)
+            {
+                //Debug.Log($"{I} - {J}");
+                if (Manager.controllers[k])
+                {
+                    if (Manager.controllers[k].Settings)
+                    {
+                        itemIndexes_s[k] = Manager.controllers[k].Settings.cells_ItemIndexes[I * Manager.Dimension_J + J];
+                    }
+                    else
+                    {
+                        Debug.LogWarning($"{Manager.controllers[k]} Settings parameter is null.");
+                    }
+                }
+                else
+                {
+                    Debug.LogWarning($"{Manager.controllers[k]} is null.");
+                }
+            }
+            itemTransforms_s = new ItemTransforms[manager.controllers.Length];
+            for (int k = 0; k < itemTransforms_s.Length; k++)
+            {
+                itemTransforms_s[k] = new ItemTransforms();
+            }
+        }
 
-//        public void Open()
-//        {
-//            _active = true;
-//            //Debug.Log($"Opened - {I}:{J}");
+        public void Open()
+        {
+            _active = true;
+            //Debug.Log($"Opened - {I}:{J}");
 
-//            for (int k = 0; k < itemIndexes_s.Length; k++)
-//            {
-//                foreach (int i in itemIndexes_s[k].Indexes)
-//                {
-//                    var controller = Manager.controllers[k];
-//                    var settings = controller.Settings;
-//                    var item = EZ_PoolManager.Spawn(controller.ItemPrefab, settings.positions[i], settings.rotations[i]);
-//                    if (controller.UseScale) item.localScale = settings.scales[i];
+            for (int k = 0; k < itemIndexes_s.Length; k++)
+            {
+                if (itemIndexes_s[k] != null)
+                {
+                    foreach (int i in itemIndexes_s[k].Indexes)
+                    {
+                        var controller = Manager.controllers[k];
+                        var settings = controller.Settings;
+                        if (controller.ItemPrefab)
+                        {
+                            var item = CKY_PoolManager.Spawn(controller.ItemPrefab, settings.positions[i], settings.rotations[i]);
+                            if (controller.UseScale) item.localScale = settings.scales[i];
 
-//                    if (item.TryGetComponent<IMatrixItem>(out var iMatrixItem))
-//                    {
-//                        iMatrixItem.ReSpawn();
-//                    }
-//                    itemTransforms_s[k].items.Add(item);
-//                }
-//            }
-//        }
+                            if (item.TryGetComponent<IMatrixItem>(out var iMatrixItem))
+                            {
+                                iMatrixItem.ReSpawn();
+                            }
+                            itemTransforms_s[k].items.Add(item);
+                        }
+                        else
+                        {
+                            Debug.LogWarning($"{controller} ItemPrefab is null.");
+                        }
+                    }
+                }
+                else
+                {
+                    Debug.LogWarning($"{itemIndexes_s} {k}. element is null.");
+                }
+            }
+        }
 
-//        public void Close()
-//        {
-//            _active = false;
-//            //Debug.Log($"Closed - {I}:{J}");
+        public void Close()
+        {
+            _active = false;
+            //Debug.Log($"Closed - {I}:{J}");
 
-//            foreach (var c in itemTransforms_s)
-//            {
-//                foreach (var item in c.items)
-//                {
-//                    EZ_PoolManager.Despawn(item);
-//                }
-//            }
+            foreach (var c in itemTransforms_s)
+            {
+                foreach (var item in c.items)
+                {
+                    CKY_PoolManager.Despawn(item);
+                }
+            }
 
-//            foreach (var c in itemTransforms_s)
-//            {
-//                c.items.Clear();
-//            }
-//        }
+            foreach (var c in itemTransforms_s)
+            {
+                c.items.Clear();
+            }
+        }
 
 
 
-//        #region Gizmos
+        #region Gizmos
 
-//        private void OnDrawGizmos()
-//        {
-//            Gizmos.color = _active ? Color.yellow : Color.red;
-//            Gizmos.DrawWireCube(transform.position, new Vector3(transform.localScale.x, 0f, transform.localScale.z));
-//        }
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = _active ? Manager.colorCell_Open : Manager.colorCell_Close;
+            Gizmos.DrawWireCube(transform.position, new Vector3(transform.localScale.x, 0f, transform.localScale.z));
+        }
 
-//        #endregion
+        #endregion
 
-//    }
-//}
+    }
+}
